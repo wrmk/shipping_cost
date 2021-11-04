8c33eafcc4b1404a24c894a1cabff8d8dde0f9ba
wrmk <elmakan@gmail.com>
2021-11-04 12:10:26 +0300

    model shipments

---
+++ db/migrate/20211104085641_create_shipments.rb
@@ -0,0 +1,14 @@
+class CreateShipments < ActiveRecord::Migration[6.1]
+  def change
+    create_table :shipments do |t|
+      t.string :length
+      t.string :width
+      t.string :height
+      t.string :volume
+      t.string :departure
+      t.string :destination
+      t.float :price
+      t.timestamps
+    end
+  end
+end
