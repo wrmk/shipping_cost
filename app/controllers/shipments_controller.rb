fe6d74825d2ed295df62b26064cb136b3149e36a
wrmk <elmakan@gmail.com>
2021-11-04 12:30:54 +0300

    get and save shipment params to db

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -1,4 +1,13 @@
 class ShipmentsController < ApplicationController
   def new
   end
+
+  def create
+    Shipment.create shipping_params
+  end
+
+  private
+  def shipping_params
+    params.permit(:length, :width, :height, :volume, :destination, :departure)
+  end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # get 'shipments/new'
   root 'shipments#new'
+  post '/' => 'shipments#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
