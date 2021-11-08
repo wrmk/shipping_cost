4b3521286534d1593b5f0f618ba8ce84efe26c79
wrmk <elmakan@gmail.com>
2021-11-08 12:20:11 +0300

    count price and save to db

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -3,11 +3,14 @@ class ShipmentsController < ApplicationController
   end
 
   def create
-    Shipment.create shipping_params
+    shipment = Shipment.new shipping_params
+    price = 500 + (shipping_params[:distance].to_f * (shipping_params[:length].to_f + shipping_params[:width].to_f + shipping_params[:height].to_f + shipping_params[:volume].to_f))
+    shipment.update(price: price)
+    shipment.save
   end
 
   private
   def shipping_params
     params.permit(:length, :width, :height, :volume, :destination, :departure, :distance)
-  end  
+  end
 end
