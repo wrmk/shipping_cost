432d29517607b4b8e387933e2ca75e2ac8fa3636
wrmk <elmakan@gmail.com>
2021-11-11 17:48:15 +0300

    render if error save

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -7,11 +7,15 @@ class ShipmentsController < ApplicationController
     shipment = Shipment.new shipping_params
     price = 500 + (shipping_params[:distance].to_f * (shipping_params[:length].to_f + shipping_params[:width].to_f + shipping_params[:height].to_f + shipping_params[:volume].to_f))
     shipment.update(price: price)
-    shipment.save
     respond_to do |format|
-      format.html { render html: "Price = #{shipment.price}" }
-      format.json { render json: shipment, only: [:price] }
-    end
+      if shipment.save
+        format.html { render html: "Price = #{shipment.price}" }
+        format.json { render json: shipment, only: [:price] }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: shipment.errors, status: :unprocessable_entity }
+      end
+    end     
   end
 
   private
